a8b2a980d51e68380f47fee254a32b9d697d0ea3
djballowe <djballowe@gmail.com>
2022-05-11 13:39:10 -0700

    starting the javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('main');
+
+
+/*
+function mainDigits(n); {
+     
+}
+*/
+
+document.querySelectorAll('.num').forEach(item => {
+        item.addEventListener('click', () => {
+            display.textContent = div.id;
+        })
+    })
+
+
+/*
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        mainDigits(button.id);
+        display.textContent = (button.id);
+    })
+})
+*/
