156191ea74a5b0b95d1fed5dfc5e452746ceaaed
djballowe <djballowe@gmail.com>
2022-05-12 15:48:38 -0700

    adding claculate functions and grabbing the 2 values

--- script.js
+++ script.js
@@ -8,24 +8,26 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         
-        
+        const start = document.getElementById('startNumber');
         const content = document.createElement('div');
         let num = document.getElementById('main').childElementCount;
         
-        if (button.className === 'factors') {
+        if (num === 1 && button.className !== 'factors') {
+            start.textContent = button.id;
+            display.appendChild(content);
+        } else if (button.className === 'factors') {
             content.textContent = button.id;
             small.appendChild(content);
-            if (num !==0) {
-                for (let i = 1; i < display.length; i++) {
-                    console.log([i]);
-                }
-            }
         } else {
             content.classList.add('mainDigit');
+            content.setAttribute('id', button.id);
             content.textContent = button.id;
             display.appendChild(content);
         }
     })
 })
+
+
+
 
 
